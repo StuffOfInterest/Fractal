0cbaa90870d04db858f4c4cad5b368845ce84735
Delbert Matlock <delbert.matlock@gmail.com>
2017-04-01 16:39:13 -0400

    Add project files.

---
+++ Fractal/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Fractal
+{
+    partial class Fractal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudDetail = new System.Windows.Forms.NumericUpDown();
+            this.butStart = new System.Windows.Forms.Button();
+            this.butStop = new System.Windows.Forms.Button();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.radStandard = new System.Windows.Forms.RadioButton();
+            this.radInvert = new System.Windows.Forms.RadioButton();
+            this.labLevels = new System.Windows.Forms.Label();
+            this.radDouble = new System.Windows.Forms.RadioButton();
+            this.radTriple = new System.Windows.Forms.RadioButton();
+            this.radSolid = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDetail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudDetail
+            // 
+            this.nudDetail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudDetail.Location = new System.Drawing.Point(59, 333);
+            this.nudDetail.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudDetail.Name = "nudDetail";
+            this.nudDetail.Size = new System.Drawing.Size(66, 20);
+            this.nudDetail.TabIndex = 1;
+            // 
+            // butStart
+            // 
+            this.butStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butStart.Location = new System.Drawing.Point(524, 330);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(75, 23);
+            this.butStart.TabIndex = 2;
+            this.butStart.Text = "Start";
+            this.butStart.UseVisualStyleBackColor = true;
+            this.butStart.Click += new System.EventHandler(this.butStart_Click);
+            // 
+            // butStop
+            // 
+            this.butStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.butStop.Enabled = false;
+            this.butStop.Location = new System.Drawing.Point(605, 330);
+            this.butStop.Name = "butStop";
+            this.butStop.Size = new System.Drawing.Size(75, 23);
+            this.butStop.TabIndex = 3;
+            this.butStop.Text = "Stop";
+            this.butStop.UseVisualStyleBackColor = true;
+            this.butStop.Click += new System.EventHandler(this.butStop_Click);
+            this.butStop.EnabledChanged += new System.EventHandler(this.butStop_EnabledChanged);
+            // 
+            // picImage
+            // 
+            this.picImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImage.Location = new System.Drawing.Point(12, 12);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(668, 312);
+            this.picImage.TabIndex = 6;
+            this.picImage.TabStop = false;
+            this.picImage.Paint += new System.Windows.Forms.PaintEventHandler(this.picImage_Paint);
+            // 
+            // radStandard
+            // 
+            this.radStandard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radStandard.AutoSize = true;
+            this.radStandard.Checked = true;
+            this.radStandard.Location = new System.Drawing.Point(151, 335);
+            this.radStandard.Name = "radStandard";
+            this.radStandard.Size = new System.Drawing.Size(68, 17);
+            this.radStandard.TabIndex = 7;
+            this.radStandard.TabStop = true;
+            this.radStandard.Text = "Standard";
+            this.radStandard.UseVisualStyleBackColor = true;
+            // 
+            // radInvert
+            // 
+            this.radInvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radInvert.AutoSize = true;
+            this.radInvert.Location = new System.Drawing.Point(225, 335);
+            this.radInvert.Name = "radInvert";
+            this.radInvert.Size = new System.Drawing.Size(52, 17);
+            this.radInvert.TabIndex = 8;
+            this.radInvert.Text = "Invert";
+            this.radInvert.UseVisualStyleBackColor = true;
+            // 
+            // labLevels
+            // 
+            this.labLevels.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labLevels.AutoSize = true;
+            this.labLevels.Location = new System.Drawing.Point(12, 335);
+            this.labLevels.Name = "labLevels";
+            this.labLevels.Size = new System.Drawing.Size(41, 13);
+            this.labLevels.TabIndex = 9;
+            this.labLevels.Text = "Levels:";
+            // 
+            // radDouble
+            // 
+            this.radDouble.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radDouble.AutoSize = true;
+            this.radDouble.Location = new System.Drawing.Point(283, 335);
+            this.radDouble.Name = "radDouble";
+            this.radDouble.Size = new System.Drawing.Size(59, 17);
+            this.radDouble.TabIndex = 10;
+            this.radDouble.TabStop = true;
+            this.radDouble.Text = "Double";
+            this.radDouble.UseVisualStyleBackColor = true;
+            // 
+            // radTriple
+            // 
+            this.radTriple.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radTriple.AutoSize = true;
+            this.radTriple.Location = new System.Drawing.Point(349, 335);
+            this.radTriple.Name = "radTriple";
+            this.radTriple.Size = new System.Drawing.Size(51, 17);
+            this.radTriple.TabIndex = 11;
+            this.radTriple.TabStop = true;
+            this.radTriple.Text = "Triple";
+            this.radTriple.UseVisualStyleBackColor = true;
+            // 
+            // radSolid
+            // 
+            this.radSolid.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.radSolid.AutoSize = true;
+            this.radSolid.Location = new System.Drawing.Point(407, 335);
+            this.radSolid.Name = "radSolid";
+            this.radSolid.Size = new System.Drawing.Size(48, 17);
+            this.radSolid.TabIndex = 12;
+            this.radSolid.TabStop = true;
+            this.radSolid.Text = "Solid";
+            this.radSolid.UseVisualStyleBackColor = true;
+            // 
+            // Fractal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 366);
+            this.Controls.Add(this.radSolid);
+            this.Controls.Add(this.radTriple);
+            this.Controls.Add(this.radDouble);
+            this.Controls.Add(this.labLevels);
+            this.Controls.Add(this.radInvert);
+            this.Controls.Add(this.radStandard);
+            this.Controls.Add(this.picImage);
+            this.Controls.Add(this.butStop);
+            this.Controls.Add(this.butStart);
+            this.Controls.Add(this.nudDetail);
+            this.MinimumSize = new System.Drawing.Size(700, 400);
+            this.Name = "Fractal";
+            this.Text = "Fractal";
+            this.Load += new System.EventHandler(this.Fractal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDetail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudDetail;
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.Button butStop;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.RadioButton radStandard;
+        private System.Windows.Forms.RadioButton radInvert;
+        private System.Windows.Forms.Label labLevels;
+        private System.Windows.Forms.RadioButton radDouble;
+        private System.Windows.Forms.RadioButton radTriple;
+        private System.Windows.Forms.RadioButton radSolid;
+    }
+}
+
---
+++ Fractal/MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Fractal : Form
+    {
+        delegate void SetStopCallback();                       // Used for thread-safe call
+        System.Drawing.Pen myPen;                              // Pen to use for drawing
+        private System.Drawing.Bitmap ImageBitmap;             // Our Bitmap declaration
+        Graphics graphicsObj;                                  // Graphics object used during drawing of image
+        private static System.Threading.Semaphore ImageLock;   // Semaphore to avoid two threads talking to the image
+        int ClockTicks = 0;                                    // Miliseconds since boot, used to keep from over invalidating
+
+        public Fractal()
+        {
+            InitializeComponent();
+        }
+
+        private void Fractal_Load(object sender, EventArgs e)
+        {
+            Graphics GraphObj;
+
+            // Setup persistent image for picture box and set background to white
+            ImageBitmap = new Bitmap(picImage.ClientRectangle.Width, picImage.ClientRectangle.Height,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            GraphObj = Graphics.FromImage(ImageBitmap);
+            GraphObj.Clear(System.Drawing.Color.White);
+            GraphObj.Dispose();
+
+            ImageLock = new System.Threading.Semaphore(1, 1);   // Create semaphore
+        }
+
+        private void butStart_Click(object sender, EventArgs e)
+        {
+            butStart.Enabled = false;
+            butStop.Enabled = true;
+            nudDetail.Enabled = false;
+            radStandard.Enabled = false;
+            radInvert.Enabled = false;
+            radDouble.Enabled = false;
+            radTriple.Enabled = false;
+            radSolid.Enabled = false;
+
+            LaunchThread();
+            //DrawFractal();
+        }
+
+        private void butStop_Click(object sender, EventArgs e)
+        {
+            EndDrawing();
+        }
+
+        private void butStop_EnabledChanged(object sender, EventArgs e)
+        {
+            // If the stop button is clicked are changed to false then reactive other controls
+            if (butStop.Enabled == false)
+            {
+                butStart.Enabled = true;
+                nudDetail.Enabled = true;
+                radStandard.Enabled = true;
+                radInvert.Enabled = true;
+                radDouble.Enabled = true;
+                radTriple.Enabled = true;
+                radSolid.Enabled = true;
+            }
+        }
+
+        private void LaunchThread()
+        {
+            System.Threading.Thread thdDraw;
+            thdDraw = new System.Threading.Thread(DrawFractal);
+            thdDraw.Start();
+        }
+
+        private void DrawFractal()
+        {
+
+            //MessageBox.Show("In Thread");
+
+            // Start a new bitmap to handle the fractal
+            ImageLock.WaitOne();
+            ImageBitmap.Dispose();
+            ImageBitmap = new Bitmap(picImage.ClientRectangle.Width, picImage.ClientRectangle.Height,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            graphicsObj = Graphics.FromImage(ImageBitmap);
+            graphicsObj.Clear(Color.White);         // Set background to white
+            ImageLock.Release();
+
+            // Create drawing tools
+            //myPen = new Pen(Color.Red);
+            //formGraphics = ImageBitmap.CreateGraphics();
+
+            // Setup area of fractal
+            double maxsize = Math.Min(picImage.Width, picImage.Height) / 2 * .8;
+
+            // Get center of image area
+            double cx = picImage.Width / 2;
+            double cy = picImage.Height / 2;
+
+            double p1x = cx + Math.Cos(Math.PI * 3 / 2) * maxsize;     // Top point
+            double p1y = cy + Math.Sin(Math.PI * 3 / 2) * maxsize;
+
+            double p2x = cx + Math.Cos(Math.PI * 1 / 6) * maxsize;     // Bottom right
+            double p2y = cy + Math.Sin(Math.PI * 1 / 6) * maxsize;
+
+            double p3x = cx + Math.Cos(Math.PI * 5 / 6) * maxsize;     // Bottom left
+            double p3y = cy + Math.Sin(Math.PI * 5 / 6) * maxsize;
+
+            // Top right
+            myPen = new Pen(Color.Red);
+            FractalLine(p1x, p1y, p2x, p2y, (int)nudDetail.Value);
+            myPen.Dispose();
+
+            // Bottom
+            myPen = new Pen(Color.Green);
+            FractalLine(p2x, p2y, p3x, p3y, (int)nudDetail.Value);
+            myPen.Dispose();
+
+            // Top left
+            myPen = new Pen(Color.Blue);
+            FractalLine(p3x, p3y, p1x, p1y, (int)nudDetail.Value);
+            myPen.Dispose();
+
+            picImage.Invalidate();              // Make sure last block of lines is displayed
+
+            // Dispose of the drawing tools
+            //myPen.Dispose();
+            graphicsObj.Dispose();
+
+            EndDrawing();
+        }
+
+        private void EndDrawing()
+        {
+            // Shut off stop button in a thread safe way
+            if (butStop.InvokeRequired)
+            {
+                SetStopCallback d = new SetStopCallback(EndDrawing);
+                this.Invoke(d, new object[] { });
+            }
+            else
+                butStop.Enabled = false;
+        }
+
+        private void FractalLine(double p1x, double p1y, double p2x, double p2y, int depth)
+        {
+            // Drop out immediately if the stop button has been clicked.
+            if (butStop.Enabled == false)
+                return;
+
+            if (depth < 1)              // If we reach 0, draw a straight line
+            {
+                ImageLock.WaitOne();
+                graphicsObj.DrawLine(myPen, (float)p1x, (float)p1y, (float)p2x, (float)p2y);
+                ImageLock.Release();
+                
+                int newticks = Environment.TickCount;
+                if (newticks - ClockTicks > 200)
+                {
+                    ClockTicks = newticks;
+                    picImage.Invalidate();
+                }
+            }
+            else
+            {
+                // Calculate line information
+                double la = Math.Atan2(p2y - p1y, p2x - p1x);  // Original line's angle
+                double ll = Math.Sqrt(Math.Pow(p2y - p1y, 2) + Math.Pow(p2x - p1x, 2));  // Original line's length
+                double na1 = la - Math.PI / 3;                // Normal angle
+                double na2 = la + Math.PI / 3;                // Inverted angle
+                double nl = ll / 3;                       // New line's length
+                //MessageBox.Show("Old Angle: " + la.ToString() + "  Old Length: " + ll.ToString() + "\n" + 
+                //                "New Angle: " + na.ToString() + "  New Length: " + nl.ToString());
+
+                // Calculate next levels points
+                double m1x = p1x + (p2x - p1x) / 3;
+                double m1y = p1y + (p2y - p1y) / 3;
+                double m2x = p1x + (p2x - p1x) * 2 / 3;
+                double m2y = p1y + (p2y - p1y) * 2 / 3;
+                double c1x = m1x + Math.Cos(na1) * nl;       // Outside (standard) point
+                double c1y = m1y + Math.Sin(na1) * nl;
+                double c2x = m1x + Math.Cos(na2) * nl;       // Inside (inverted) point
+                double c2y = m1y + Math.Sin(na2) * nl;
+                double i1x = p1x + Math.Cos(na2) * nl;       // Inside front fill point
+                double i1y = p1y + Math.Sin(na2) * nl;
+                double i2x = m2x + Math.Cos(na2) * nl;       // Inside back fill point
+                double i2y = m2y + Math.Sin(na2) * nl;
+                double o1x = p1x + Math.Cos(na1) * nl;       // Outside front fill point
+                double o1y = p1y + Math.Sin(na1) * nl;
+                double o2x = m2x + Math.Cos(na1) * nl;       // Outside front fill point
+                double o2y = m2y + Math.Sin(na1) * nl;
+
+                FractalLine(p1x, p1y, m1x, m1y, depth - 1);
+                if (radTriple.Checked || radSolid.Checked)
+                    FractalLine(m1x, m1y, m2x, m2y, depth - 1);    // Triple barrel hack
+                if (radStandard.Checked || radDouble.Checked || radTriple.Checked || radSolid.Checked)
+                    FractalLine(m1x, m1y, c1x, c1y, depth - 1);
+                if (radInvert.Checked || radDouble.Checked || radTriple.Checked || radSolid.Checked)
+                    FractalLine(m1x, m1y, c2x, c2y, depth - 1);    // Double barrel hack
+                if (radStandard.Checked || radDouble.Checked || radTriple.Checked || radSolid.Checked)
+                    FractalLine(c1x, c1y, m2x, m2y, depth - 1);
+                if (radInvert.Checked || radDouble.Checked || radTriple.Checked || radSolid.Checked)
+                    FractalLine(c2x, c2y, m2x, m2y, depth - 1);    // Double barrel hack
+                FractalLine(m2x, m2y, p2x, p2y, depth - 1);
+                if (radSolid.Checked)
+                {
+                    FractalLine(m2x, m2y, i2x, i2y, depth - 1);    // Inside fill back
+                    FractalLine(p1x, p1y, o1x, o1y, depth - 1);    // Front outside
+                    FractalLine(o1x, o1y, c1x, c1y, depth - 1);
+                    FractalLine(o1x, o1y, m1x, m1y, depth - 1);
+                    FractalLine(m2x, m2y, o2x, o2y, depth - 1);    // Back outside
+                    FractalLine(c1x, c1y, o2x, o2y, depth - 1);
+                    FractalLine(o2x, o2y, p2x, p2y, depth - 1);
+                }
+            }
+        }
+
+        private void picImage_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphObj = e.Graphics;
+            ImageLock.WaitOne();
+            graphObj.DrawImage(ImageBitmap, 0, 0, ImageBitmap.Width, ImageBitmap.Height);
+            ImageLock.Release();
+        }
+
+
+    }
+}
